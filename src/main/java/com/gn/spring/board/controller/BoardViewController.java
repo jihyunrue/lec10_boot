4518c75b1d54cb03ba6d0111b92ff282da7d47fb
jhrue <jhrue7@gmail.com>
2024-08-08 14:36:51 +0900

    [류지현] Paging 로직 구성

---
+++ src/main/java/com/gn/spring/board/controller/BoardViewController.java
@@ -0,0 +1,39 @@
+package com.gn.spring.board.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.gn.spring.board.domain.BoardDto;
+import com.gn.spring.board.service.BoardService;
+
+@Controller
+public class BoardViewController {
+	
+	private final BoardService boardService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(BoardViewController.class);
+	
+	@Autowired
+	public BoardViewController(BoardService boardService) {
+		this.boardService = boardService;
+	}
+	
+	
+	@GetMapping("/board")
+	public String selectBoardList(Model model,
+			@PageableDefault(page = 0, size = 10, sort = "regDate", direction = Sort.Direction.DESC) 
+			Pageable pageable) {
+		
+		Page<BoardDto> resultList = boardService.selectBoardList(pageable);
+		LOGGER.debug(resultList.toString());
+		model.addAttribute("resultList",resultList);
+		return "board/list";
+	}
+}
---
+++ src/main/java/com/gn/spring/board/service/BoardService.java
@@ -0,0 +1,51 @@
+package com.gn.spring.board.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.gn.spring.board.domain.Board;
+import com.gn.spring.board.domain.BoardDto;
+import com.gn.spring.board.repository.BoardRepository;
+
+@Service
+public class BoardService {
+	
+	private final BoardRepository boardRepository;
+	
+	@Autowired
+	public BoardService(BoardRepository boardRepository) {
+		this.boardRepository = boardRepository;
+	}
+	
+	public Page<BoardDto> selectBoardList(Pageable pageable){
+		
+		Page<Board> boardList = boardRepository.findAll(pageable);
+		
+		List<BoardDto> boardDtoList = new ArrayList<BoardDto>();
+		for(Board b : boardList) {
+			BoardDto dto = new BoardDto().toDto(b);
+			boardDtoList.add(dto);
+		}
+		
+			
+		return new PageImpl<>(boardDtoList, pageable, boardList.getTotalElements());
+		
+		
+//		List<Board> boardList = boardRepository.findAll();
+//		List<BoardDto> boardDtoList = new ArrayList<BoardDto>();
+//		// Board(Entity)를 BoardDto로 바꿔주기
+//		for(Board board: boardList) {
+//			BoardDto boardDto = new BoardDto().toDto(board);
+//			boardDtoList.add(boardDto);
+//		}
+//		return boardDtoList;
+		
+	}
+	
+}
